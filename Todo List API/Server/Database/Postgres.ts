22bb43ed0b15a231c1a569e9c709dce3ebf28768
Muchiri <llwmuchiri@gmail.com>
2025-06-29 10:17:23 +0300

    Handling of user deletion requests

--- Todo List API/Server/Database/Postgres.ts
+++ Todo List API/Server/Database/Postgres.ts
@@ -26,9 +26,9 @@ export class Database {
 
       if (table == "user")
         rows = await this.Client.query("SELECT * FROM users");
-      else await this.Client.query("SELECT * FROM todos");
+      else rows = await this.Client.query("SELECT * FROM todos");
 
-      if (Array.isArray(rows)) return true;
+      if (rows) return true;
 
       return false;
     } catch (error) {
@@ -39,7 +39,7 @@ export class Database {
   async registration(
     details: UserDetails
   ): Promise<string | SecurityCredentials> {
-    if (!(await this.tableExists("user"))) {
+    if ((await this.tableExists("user")) == false) {
       try {
         await this.Client.query(
           "CREATE TABLE users(id UUID DEFAULT uuid_generate_v4(), name VARCHAR(100), email VARCHAR(100) UNIQUE, password TEXT);"
@@ -54,7 +54,7 @@ export class Database {
         "INSERT INTO users(name,email,password) VALUES($1,$2,$3);",
         [details.name, details.email, details.password]
       );
-
+      console.log("Here at token");
       return generateToken(
         (
           await this.Client.query("SELECT * FROM users WHERE email=$1", [
@@ -70,7 +70,7 @@ export class Database {
   async login(
     details: Omit<UserDetails, "name">
   ): Promise<UserDetails | string | SecurityCredentials> {
-    if (!(await this.tableExists("user")))
+    if ((await this.tableExists("user")) == false)
       try {
         await this.Client.query(
           "CREATE TABLE users(id UUID DEFAULT uuid_generate_v4(), name VARCHAR(100), email VARCHAR(100) UNIQUE, password TEXT);"
@@ -100,6 +100,49 @@ export class Database {
     }
   }
 
+  async updateUser(
+    id: string,
+    newDetails: Partial<Omit<UserDetails, "id">>
+  ): Promise<boolean | Error | string> {
+    try {
+      let userChecker = await this.Client.query(
+        `SELECT * FROM users where id::text=${id}`
+      );
+
+      if (userChecker.rowCount && userChecker.rowCount <= 0)
+        return "User does not exist";
+
+      for (let [key, value] of Object.entries(newDetails)) {
+        await this.Client.query(
+          `UPDATE users WHERE id::text=${id} SET ${key}=${value}`
+        );
+      }
+
+      return true;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async deleteUser(id: string): Promise<Error | boolean | string> {
+    try {
+      let userChecker = await this.Client.query(
+        `SELECT * FROM users where id::text=$1;`,
+        [id]
+      );
+
+      if (userChecker.rowCount && userChecker.rowCount <= 0) {
+        console.log("Here");
+        return "User does not exist";
+      }
+
+      await this.Client.query(`DELETE FROM users where id::text=$1;`, [id]);
+      return true;
+    } catch (error) {
+      return error;
+    }
+  }
+
   //Todo lists
   async createTodo(
     token: string,
@@ -111,22 +154,22 @@ export class Database {
 
     if (!(await this.tableExists("todos")))
       await this.Client.query(
-        "CREATE TABLE todos (id DEFAULT uuid_generate_v4(),userId uuid REFERENCES user(id),title TEXT, description TEXT);"
+        "CREATE TABLE todos (id uuid DEFAULT uuid_generate_v4(),userId uuid REFERENCES users(id),title TEXT, description TEXT);"
       );
 
     if (title.length > 0 && description.length > 0) {
-      const decodedUser: UserDetails = JSON.parse(user as string);
+      // const decodedUser: UserDetails = JSON.parse(user as string);
 
       await this.Client.query(
         "INSERT INTO todos(title,description,userId) VALUES($1,$2,$3);",
-        [title, description, decodedUser.id]
+        [title, description, (user as any).id]
       );
     } else return "incomplete credentials";
 
     return true;
   }
 
-  async getTodos(token: string): Promise<string | any> {
+  async getTodos(token: string): Promise<string | QueryResult> {
     const user = verifyUser(token);
 
     if (typeof user == "boolean") return "Token passed is invalid";
@@ -135,7 +178,8 @@ export class Database {
       if (!(await this.tableExists("todos"))) return "Table non existent";
 
       return await this.Client.query(
-        `SELECT * FROM todos WHERE userId::text=${(user as UserDetails).id}`
+        `SELECT * FROM todos WHERE userId::text=$1`,
+        [(user as UserDetails).id]
       );
     } catch (error) {
       return (error as Error).message;
@@ -156,12 +200,11 @@ export class Database {
 
     if (!id) return "Id should be provided";
 
-    for (let [key, value] of Object.entries(
-      Object.create({ title, description })
-    )) {
+    for (let [key, value] of Object.entries({ title, description })) {
       if (value && (value as string).length > 0)
         await this.Client.query(
-          `UPDATE todos WHERE id::text=${id} SET ${key}=${value}`
+          `UPDATE todos SET ${key}=$1 WHERE id::text=$2`,
+          [value, id]
         );
     }
 
@@ -170,29 +213,30 @@ export class Database {
 
   async deleteTodo({
     token,
-    title,
+    todoId,
   }: {
     token: string;
-    title: string;
-  }): Promise<string | any> {
+    todoId: string;
+  }): Promise<string | Error> {
     const user = verifyUser(token);
 
     if (typeof user == "boolean") return "Token passed is invalid";
 
-    if (!token || !title) return "Incomplete credentials";
+    if (!token || !todoId) return "Incomplete credentials";
 
     try {
       if (!(await this.tableExists("todos"))) return "Table non-existent";
 
-      await this.Client.query(
-        `DELETE FROM todos where title=${title} AND id::text=${
-          (user as UserDetails).id
-        }`
+      let deletion = await this.Client.query(
+        `DELETE FROM todos where id::text=$1 AND userid::text=$2;`,
+        [todoId, (user as UserDetails).id]
       );
+
+      if (deletion.rowCount === 0) return "Row does not exist";
 
       return "Delete successful";
     } catch (error) {
-      return (error as Error).message;
+      return error as Error;
     }
   }
 }
